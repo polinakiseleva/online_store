2f4872516168449e165a521dcb5875d5dee9041b
Polina <polinakiseleva4@gmail.com>
2023-07-13 15:21:09 +0500

    Внесла изменения в настройки подключения, обновила список зависимостей

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online_store',
+        'USER': 'postgres'
     }
 }
 
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
