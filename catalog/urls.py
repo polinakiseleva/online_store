928d3cd41e81c1912a981c206153ed5660dcf8fd
Polina <polinakiseleva4@gmail.com>
2023-07-18 15:18:34 +0500

    Настройка проекта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,11 @@
-from catalog.views import home, contact_inf
+from catalog.apps import CatalogConfig
+from catalog.views import home, contact_inf, product
 from django.urls import path
 
+app_name = CatalogConfig.name
+
 urlpatterns = [
-    path('', home),
-    path('contacts/', contact_inf)
+    path('', home, name='home'),
+    path('contacts/', contact_inf, name='contacts'),
+    path('product/', product, name='product'),
 ]
