e5b2670c45dea1997f81e69c0eccfa0a3bebe18d
Polina <polinakiseleva4@gmail.com>
2023-07-18 15:21:47 +0500

    Добавила context

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()[:3],
+        'title': 'Homepage',
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contact_inf(request):
-    return render(request, 'catalog/contact_inf.html')
+    context = {
+        'title': 'Contacts',
+    }
+    return render(request, 'catalog/contact_inf.html', context)
+
+
+def product(request):
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'All categories products',
+    }
+    return render(request, 'catalog/product.html', context)
