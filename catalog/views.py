a45e33cd8c9e12194a3e81fa781390175109ccf7
Polina <polinakiseleva4@gmail.com>
2023-07-26 05:04:15 +0500

    Перевела имеющиеся контроллеры с FBV на CBV.

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def home(request):
-    context = {
-        'object_list': Product.objects.all()[:3],
-        'title': 'Homepage',
-    }
-    return render(request, 'catalog/home.html', context)
+class HomeListView(ListView):
+    model = Product
+    template_name = 'catalog/home.html'
+    extra_context = {'title': 'Homepage', }
+
+    def get_queryset(self):
+        return Product.objects.all()[:3]
 
 
 def contact_inf(request):
@@ -18,9 +20,12 @@ def contact_inf(request):
     return render(request, 'catalog/contact_inf.html', context)
 
 
-def product(request):
-    context = {
-        'object_list': Product.objects.all(),
-        'title': 'All categories products',
-    }
-    return render(request, 'catalog/product.html', context)
+class ProductListView(ListView):
+    model = Product
+    template_name = 'catalog/product.html'
+    extra_context = {'title': 'All categories products', }
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'catalog/product_detail.html'
