9f8e96697d32dc62c4af25978c14937739124dc6
Polina <polinakiseleva4@gmail.com>
2023-07-26 05:08:41 +0500

    Обновила ссылки на контроллеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact_inf, product
+from catalog.views import contact_inf, HomeListView, ProductListView, ProductDetailView
 from django.urls import path
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', HomeListView.as_view(), name='home'),
     path('contacts/', contact_inf, name='contacts'),
-    path('product/', product, name='product'),
+    path('product/', ProductListView.as_view(), name='product'),
+    path('view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
