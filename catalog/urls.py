165701e60334c658a590b852e2f60a9ac05f8ffd
Polina <polinakiseleva4@gmail.com>
2023-08-04 03:13:54 +0500

    Добавила урлы на добавление и редактирование продукта

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,6 @@
 from catalog.apps import CatalogConfig
-from catalog.views import contact_inf, HomeListView, ProductListView, ProductDetailView
+from catalog.views import contact_inf, HomeListView, ProductListView, ProductDetailView, \
+    ProductCreateView, ProductUpdateView
 from django.urls import path
 
 app_name = CatalogConfig.name
@@ -9,4 +10,6 @@ urlpatterns = [
     path('contacts/', contact_inf, name='contacts'),
     path('product/', ProductListView.as_view(), name='product'),
     path('view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='products_update'),
 ]
